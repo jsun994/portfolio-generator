e0e09099cdd4bf5694d8b141b22b8379d08912fe
jsun <jaysun054@gmail.com>
2021-09-23 10:46:01 -0700

    9 2

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 const profileDataArgs = (process.argv.slice(2, process.argv.length));
-//console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
+const generatePage = () => 'Name: Jane, Github: janehub';
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+console.log(generatePage());
